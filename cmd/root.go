be9594324de0854657996b2a21977d7b1f0600d1
Suhun Han <ssut@ssut.me>
2021-01-17 20:26:42 +0900

    feat: cli

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"errors"
 	"net/url"
 
 	"github.com/spf13/cobra"
@@ -8,15 +9,37 @@ import (
 	"github.com/ssut/wwwsync/www"
 )
 
+const (
+	DefaultIndexFetchWorkerCount = 32
+	DefaultDownloadWorkerCount   = 8
+	DefaultTargetDirectory       = "out"
+)
+
 var (
+	indexFetchWorkerCount int
+	downloadWorkerCount   int
+	targetDirectory       string
+
 	rootCmd = &cobra.Command{
+		Args: func(_ *cobra.Command, args []string) error {
+			if len(args) < 1 {
+				return errors.New("Must pass a URL")
+			}
+
+			return nil
+		},
 		Run: func(cmd *cobra.Command, args []string) {
-			baseURL, _ := url.Parse("https://dl-cdn.alpinelinux.org")
+			targetURL := args[1]
+			baseURL, err := url.Parse(targetURL)
+			if err != nil {
+				panic(err)
+			}
+
 			options := www.ClientOptions{
 				BaseURL:               *baseURL,
-				IndexFetchWorkerCount: 32,
-				DownloadWorkerCount:   8,
-				TargetDirectory:       "./out",
+				IndexFetchWorkerCount: indexFetchWorkerCount,
+				DownloadWorkerCount:   downloadWorkerCount,
+				TargetDirectory:       targetDirectory,
 			}
 			client := www.NewClient(options)
 			client.Start()
@@ -30,4 +53,8 @@ func Execute() error {
 
 func init() {
 	viper.SetDefault("license", "MIT")
+
+	rootCmd.Flags().IntVarP(&indexFetchWorkerCount, "index-workers", "i", indexFetchWorkerCount, "Index fetch worker count")
+	rootCmd.Flags().IntVarP(&downloadWorkerCount, "download-workers", "d", downloadWorkerCount, "Download worker count")
+	rootCmd.Flags().StringVarP(&targetDirectory, "target-directory", "o", DefaultTargetDirectory, "Target output directory")
 }
